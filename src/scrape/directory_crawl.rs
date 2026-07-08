70eb7a3660e494e6ca55019f434e874dbe0f2de7
Griffin Pundt <116926805+gpundt@users.noreply.github.com>
2026-07-08 08:34:22 -0400

    beginning directory crawl using wordlist

--- src/scrape/directory_crawl.rs
+++ src/scrape/directory_crawl.rs
@@ -1,12 +1,12 @@
+use super::connectivity_check;
 use log::{debug, error, info, trace, warn};
 use std::fs::File;
 use std::io::{BufRead, BufReader};
 
-
 pub fn parse_word_list(filepath: &String) -> Result<Vec<String>, String> {
     let file = match File::open(filepath) {
         Ok(file) => file,
-        Err(e) => return Err(e.to_string())
+        Err(e) => return Err(e.to_string()),
     };
     let reader = BufReader::new(file);
     let mut word_list: Vec<String> = Vec::new();
@@ -14,22 +14,35 @@ pub fn parse_word_list(filepath: &String) -> Result<Vec<String>, String> {
     for line_result in reader.lines() {
         let line = match line_result {
             Ok(line_string) => line_string,
-            Err(e) => return Err(e.to_string())
+            Err(e) => return Err(e.to_string()),
         };
 
-        if !line.trim().is_empty(){
+        if !line.trim().is_empty() {
             word_list.push(line);
-        }            
+        }
     }
 
     info!("{} Words Parsed from '{}'", word_list.len(), filepath);
     Ok(word_list)
 }
 
-///
+/// Using wordlist, contacts each URL to see if it exists
 pub fn directory_crawl(url: &String, wordlist: Vec<String>) -> Result<(), String> {
     info!("Beginning Directory Crawl on '{}'", url);
-    
+
+    for p in wordlist {
+        let page_path: String = p;
+
+        // Strip '/' from beginning of path
+        let formatted_path = page_path.strip_prefix("/").unwrap_or(&page_path);
+
+        let current_url: String = format!("{}/{}", url, formatted_path);
+
+        match connectivity_check::validate(&current_url) {
+            Ok(_) => info!("{:<40} Found", current_url),
+            Err(err) => warn!("{:<40} {}", current_url, err),
+        }
+    }
 
     Ok(())
 }
